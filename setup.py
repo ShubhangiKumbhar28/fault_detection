84781e148304c32d717cd99bdaf61075dcc329b0
Shubhangi Kumbhar <Shubhangi2728@gmail.com>
2023-12-06 18:01:28 +0530

    updated pipeline

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from setuptools import find_packages, setup
 from typing import List
 
 REQUAIREMENT_FILE_NAME="requirements.txt"
-HYPEN_E_DOT = "-e."
-def get_requirements() ->List[str]:
-    with open(REQUAIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readline()
-        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+HYPEN_E_DOT = "-e ."
+def get_requirements()->List[str]:
 
-        if HYPEN_E_DOT in requirement_list:
-            requirement_list.remove(HYPEN_E_DOT)
-        return requirement_list
+    with open(REQUAIREMENT_FILE_NAME) as requirement_file:
+        requirements_list = requirement_file.readlines()
+    requirements_list = [requirement_name.replace("\n","") for requirement_name in requirements_list]
+
+    if HYPEN_E_DOT in requirements_list:
+        requirements_list.remove(HYPEN_E_DOT)
+    return requirements_list
 
 
 setup(
     name="sensor",
-    version="0.0.1",
-    auther="shubhangi",
+    version="0.0.2",
+    author="shubhangi",
     author_email="shubhangikumbhar2208@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements(),
